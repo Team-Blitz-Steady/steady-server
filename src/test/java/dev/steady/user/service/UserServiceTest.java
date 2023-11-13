f29d33425a8ec6e5467f2e26b1a5274969084805
na-yk <ninghao17@naver.com>
2023-11-13 13:09:35 +0900

    :white_check_mark: 내 프로필 조회 기능 Service 테스트 추가

--- src/test/java/dev/steady/user/service/UserServiceTest.java
+++ src/test/java/dev/steady/user/service/UserServiceTest.java
@@ -1,21 +1,28 @@
 package dev.steady.user.service;
 
+import dev.steady.user.domain.Position;
 import dev.steady.user.domain.Stack;
 import dev.steady.user.domain.User;
+import dev.steady.user.domain.UserStack;
 import dev.steady.user.domain.repository.PositionRepository;
 import dev.steady.user.domain.repository.StackRepository;
 import dev.steady.user.domain.repository.UserRepository;
 import dev.steady.user.domain.repository.UserStackRepository;
 import dev.steady.user.dto.request.UserCreateRequest;
+import dev.steady.user.dto.response.UserDetailResponse;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.support.TransactionTemplate;
 
+import java.util.List;
 import java.util.stream.IntStream;
 
+import static dev.steady.global.auth.AuthFixture.createUserInfo;
+import static dev.steady.user.fixture.UserFixtures.createFirstUser;
 import static dev.steady.user.fixture.UserFixtures.createPosition;
 import static dev.steady.user.fixture.UserFixtures.createStack;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -42,6 +49,13 @@ class UserServiceTest {
     @Autowired
     private TransactionTemplate transactionTemplate;
 
+    private Position position;
+
+    @BeforeEach
+    void setUp() {
+        this.position = positionRepository.save(createPosition());
+    }
+
     @AfterEach
     void tearDown() {
         userStackRepository.deleteAll();
@@ -50,11 +64,10 @@ class UserServiceTest {
         stackRepository.deleteAll();
     }
 
-    @DisplayName("프로필 정보를 입력 받아 유저를 생성한다.")
     @Test
+    @DisplayName("프로필 정보를 입력 받아 유저를 생성한다.")
     void createUser() {
         // given
-        var savedPosition = positionRepository.save(createPosition());
         var stacks = IntStream.range(0, 3)
                 .mapToObj(i -> createStack())
                 .toList();
@@ -62,7 +75,7 @@ class UserServiceTest {
         var stackIds = savedStacks.stream().map(Stack::getId).toList();
 
         // when
-        var request = new UserCreateRequest(1L, "Nickname", savedPosition.getId(), stackIds);
+        var request = new UserCreateRequest(1L, "Nickname", position.getId(), stackIds);
         var userId = userService.createUser(request);
 
         User user = transactionTemplate.execute(status -> {
@@ -74,8 +87,34 @@ class UserServiceTest {
 
         assertAll(
                 () -> assertThat(user.getId()).isEqualTo(userId),
-                () -> assertThat(user.getPosition().getName()).isEqualTo(savedPosition.getName()),
+                () -> assertThat(user.getPosition().getName()).isEqualTo(position.getName()),
                 () -> assertThat(userStacks).hasSameSizeAs(request.stackIds())
+        );
+    }
+
+    @Test
+    @DisplayName("내 프로필을 조회할 수 있다.")
+    void getMyUserDetail() {
+        // given
+        var savedUser = userRepository.save(createFirstUser(position));
+        var userInfo = createUserInfo(savedUser.getId());
+
+        // when
+        UserDetailResponse response = userService.getMyUserDetail(userInfo);
+        User user = transactionTemplate.execute(status -> {
+            var foundUser = userRepository.findById(userInfo.userId()).get();
+            foundUser.getPosition().getName();
+            return foundUser;
+        });
+        List<UserStack> userStacks = userStackRepository.findAllByUser(user);
+
+        // then
+        assertAll(
+                () -> assertThat(response.userId()).isEqualTo(user.getId()),
+                () -> assertThat(response.nickname()).isEqualTo(user.getNickname()),
+                () -> assertThat(response.profileImage()).isEqualTo(user.getProfileImage()),
+                () -> assertThat(response.position().name()).isEqualTo(user.getPosition().getName()),
+                () -> assertThat(response.stacks()).hasSameSizeAs(userStacks)
         );
     }
 
