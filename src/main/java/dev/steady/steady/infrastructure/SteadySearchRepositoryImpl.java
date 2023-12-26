61b66aab4b35dae209b0303b70dcc48473cf2f1f
weonest <geonhee33@naver.com>
2023-12-26 21:43:20 +0900

    :recycle: Querydsl 필터링 로직 커서 기반 No-Offset 방식으로 변경

--- src/main/java/dev/steady/steady/infrastructure/SteadySearchRepositoryImpl.java
+++ src/main/java/dev/steady/steady/infrastructure/SteadySearchRepositoryImpl.java
@@ -3,37 +3,27 @@ package dev.steady.steady.infrastructure;
 import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.BooleanExpression;
-import com.querydsl.core.types.dsl.DatePath;
-import com.querydsl.core.types.dsl.DateTimePath;
-import com.querydsl.core.types.dsl.Expressions;
-import com.querydsl.core.types.dsl.NumberPath;
-import com.querydsl.core.types.dsl.StringPath;
-import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import com.querydsl.jpa.sql.JPASQLQuery;
-import com.querydsl.sql.SQLExpressions;
-import com.querydsl.sql.SQLTemplates;
 import dev.steady.global.auth.UserInfo;
 import dev.steady.steady.domain.Participant;
 import dev.steady.steady.domain.Steady;
 import dev.steady.steady.domain.SteadyStatus;
 import dev.steady.steady.dto.FilterConditionDto;
 import dev.steady.steady.dto.response.MySteadyQueryResponse;
+import dev.steady.steady.uitl.Cursor;
 import dev.steady.user.domain.User;
-import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.domain.SliceImpl;
-import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
 
 import static dev.steady.steady.domain.QParticipant.participant;
 import static dev.steady.steady.domain.QSteady.steady;
@@ -52,57 +42,24 @@ import static dev.steady.steady.infrastructure.util.DynamicQueryUtils.orderBySor
 public class SteadySearchRepositoryImpl implements SteadySearchRepository {
 
     private final JPAQueryFactory jpaQueryFactory;
-    private final EntityManager entityManager;
-    private final SQLTemplates sqlTemplates;
 
     @Override
     public Page<Steady> findAllByFilterCondition(UserInfo userInfo, FilterConditionDto condition, Pageable pageable) {
-        JPASQLQuery<?> jpaSqlQuery = new JPASQLQuery<>(entityManager, sqlTemplates);
-
-        StringPath subQueryAlias = Expressions.stringPath("sub_query_steady");
-        NumberPath<Long> subQueryId = Expressions.numberPath(Long.class, subQueryAlias, "id");
-
-        DateTimePath<LocalDateTime> promotedAt = Expressions.dateTimePath(LocalDateTime.class, steady, "promoted_at");
-        DatePath<LocalDate> deadline = Expressions.datePath(LocalDate.class, steady, "deadline");
-
-        NumberPath<Long> steadyLikeSteadyId = Expressions.numberPath(Long.class, steadyLike, "steady_id");
-        NumberPath<Long> steadyStackSteadyId = Expressions.numberPath(Long.class, steadyStack, "steady_id");
-        NumberPath<Long> steadyPositionSteadyId = Expressions.numberPath(Long.class, steadyPosition, "steady_id");
-
-        List<Steady> steadies = jpaSqlQuery
-                .select(steady)
+        List<Steady> steadies = jpaQueryFactory
+                .selectFrom(steady)
                 .distinct()
-                .from(steady)
-                .innerJoin(
-                        SQLExpressions.select(steady.id, promotedAt, deadline).distinct()
-                                .from(steady)
-                                .leftJoin(steadyLike).on(steady.id.eq(steadyLikeSteadyId))
-                                .where(isLikedSteady(condition.like(), userInfo))
-                                .orderBy(orderBySort(pageable.getSort(), Steady.class))
-                                .offset(pageable.getOffset())
-                                .limit(pageable.getPageSize()),
-                        subQueryAlias
-                )
-                .on(steady.id.eq(subQueryId))
-                .innerJoin(steadyStack)
-                .on(steady.id.eq(steadyStackSteadyId))
-                .innerJoin(steadyPosition)
-                .on(steady.id.eq(steadyPositionSteadyId))
-                .fetch();
-
-        JPAQuery<Long> count = jpaQueryFactory
-                .select(steady.count())
-                .distinct()
-                .from(steady)
                 .innerJoin(steadyStack)
                 .on(steady.id.eq(steadyStack.steady.id))
                 .innerJoin(steadyPosition)
                 .on(steady.id.eq(steadyPosition.steady.id))
                 .leftJoin(steadyLike)
                 .on(steady.id.eq(steadyLike.steady.id))
-                .where(searchCondition(userInfo, condition));
+                .where(filterConditionBuilder(userInfo, condition))
+                .orderBy(orderBySort(pageable.getSort(), Steady.class))
+                .limit(pageable.getPageSize())
+                .fetch();
 
-        return PageableExecutionUtils.getPage(steadies, pageable, count::fetchCount);
+        return new PageImpl<>(steadies, pageable, steadies.size());
     }
 
     @Override
@@ -156,8 +113,15 @@ public class SteadySearchRepositoryImpl implements SteadySearchRepository {
         return null;
     }
 
-    private BooleanBuilder searchCondition(UserInfo userInfo, FilterConditionDto condition) {
+    private BooleanBuilder filterConditionBuilder(UserInfo userInfo, FilterConditionDto condition) {
         BooleanBuilder booleanBuilder = new BooleanBuilder();
+
+        Cursor cursor = condition.cursor();
+        if (Objects.isNull(cursor.getPromotedAt())) {
+            booleanBuilder.and(filterCondition(cursor.getDeadline(), steady.deadline::goe));
+        }
+        booleanBuilder.and(filterCondition(cursor.getPromotedAt(), steady.promotion.promotedAt::lt));
+
         booleanBuilder.and(filterCondition(condition.steadyType(), steady.type::eq));
         booleanBuilder.and(filterCondition(condition.steadyMode(), steady.steadyMode::eq));
         booleanBuilder.and(filterCondition(condition.stacks(), steadyStack.stack.name::in));
@@ -170,14 +134,8 @@ public class SteadySearchRepositoryImpl implements SteadySearchRepository {
             return booleanBuilder.and(filterCondition(condition.keyword(), steady.title::contains))
                     .or(filterCondition(condition.keyword(), steady.content::contains));
         }
-        return booleanBuilder;
-    }
 
-    private BooleanExpression isLikedSteady(boolean like, UserInfo userInfo) {
-        if (like) {
-            return Expressions.numberPath(Long.class, steadyLike, "user_id").eq(userInfo.userId());
-        }
-        return null;
+        return booleanBuilder;
     }
 
 }
