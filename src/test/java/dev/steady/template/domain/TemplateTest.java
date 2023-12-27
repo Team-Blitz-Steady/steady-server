4fabd785e726eb23f06e697da7fb9eec48465698
weonest <geonhee33@naver.com>
2023-12-27 20:43:59 +0900

    :white_check_mark: TemplateTest V2로 변경

--- src/test/java/dev/steady/template/domain/TemplateTest.java
+++ src/test/java/dev/steady/template/domain/TemplateTest.java
@@ -2,13 +2,13 @@ package dev.steady.template.domain;
 
 import dev.steady.global.exception.ForbiddenException;
 import dev.steady.user.domain.User;
-import dev.steady.user.fixture.UserFixtures;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.test.util.ReflectionTestUtils;
 
 import static dev.steady.template.fixture.TemplateFixture.createTemplate;
-import static dev.steady.user.fixture.UserFixtures.createSecondUser;
+import static dev.steady.user.fixture.UserFixturesV2.generatePosition;
+import static dev.steady.user.fixture.UserFixturesV2.generateUser;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class TemplateTest {
@@ -16,12 +16,12 @@ class TemplateTest {
     @DisplayName("템플릿의 생성자가 아니면 예외가 발생한다.")
     @Test
     void validateOwnerTest() {
-        var position = UserFixtures.createPosition();
-        User user = UserFixtures.createFirstUser(position);
+        var position = generatePosition();
+        User user = generateUser(position);
         ReflectionTestUtils.setField(user, "id", 1L);
         Template template = createTemplate(user);
 
-        assertThatThrownBy(() -> template.validateOwner(createSecondUser(position)))
+        assertThatThrownBy(() -> template.validateOwner(generateUser(position)))
                 .isInstanceOf(ForbiddenException.class);
     }
 
