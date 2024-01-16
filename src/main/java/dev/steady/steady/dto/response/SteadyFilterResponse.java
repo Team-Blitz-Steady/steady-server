1488d3e58c9bcf4129af41ac4414a6e20100d124
weonest <geonhee33@naver.com>
2024-01-16 23:21:55 +0900

    :recycle: SteadyFilterResponse 필드 추가 및 가드 패턴 적용

--- src/main/java/dev/steady/steady/dto/response/SteadyFilterResponse.java
+++ src/main/java/dev/steady/steady/dto/response/SteadyFilterResponse.java
@@ -4,8 +4,24 @@ import dev.steady.steady.domain.Steady;
 
 import java.util.List;
 
-public record SteadyFilterResponse (
+public record SteadyFilterResponse(
         List<Steady> result,
-        Steady prevCursor
+        Steady prevCursorSteady,
+        Steady nextCursorSteady
 ) {
+
+    public SteadyFilterResponse(List<Steady> result, Steady prevCursorSteady, Steady nextCursorSteady) {
+        this.result = result;
+        this.prevCursorSteady = prevCursorSteady;
+        if (result.isEmpty()) {
+            this.nextCursorSteady = null;
+        }else {
+            this.nextCursorSteady = result.get(result.size() - 1);
+        }
+    }
+
+    public SteadyFilterResponse(List<Steady> result, Steady prevCursor) {
+        this(result, prevCursor, null);
+    }
+
 }
